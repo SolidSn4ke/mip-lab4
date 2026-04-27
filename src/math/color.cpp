2f059dfff5b2340cc3dfe61cd674dbe4813e4914
vvlaads <vvlaads@gmail.com>
2026-04-27 12:48:10 +0300

    Refactored: added operators for color

--- src/math/color.cpp
+++ src/math/color.cpp
@@ -4,6 +4,16 @@
 Color::Color(float r, float g, float b) : r(r), g(g), b(b) {}
 
 // Конвертация в вектор
-Vector Color::toVector() {
+Vector Color::toVector() const {
     return Vector(r, g, b);
+}
+
+// Сложение цветов
+Color Color::operator+(const Color& other) const {
+    return Color(r + other.r, g + other.g, b + other.b);
+}
+
+// Умножение на число
+Color Color::operator*(float k) const {
+    return Color(r * k, g * k, b * k);
 }
--- src/math/color.h
+++ src/math/color.h
@@ -7,8 +7,14 @@ class Color {
     float r, g, b;
 
     // Конструктор
-    Color(float r, float g, float b);
+    Color(float r = 0, float g = 0, float b = 0);
 
     // Конвертация в вектор
-    Vector toVector();
+    Vector toVector() const;
+
+    // Сложение цветов
+    Color operator+(const Color& other) const;
+
+    // Умножение на число
+    Color operator*(float k) const;
 };
