37c77163eee0752be782610fa70890a41f6f6253
vvlaads <vvlaads@gmail.com>
2026-05-11 14:33:26 +0300

    Removed legacy material class

--- src/math/material.cpp
+++
@@ -1,6 +0,0 @@
-#include "material.h"
-#include <algorithm>
-
-// Конструктор
-Material::Material(const Color& diffuse, const Color& specular, float shininess)
-    : diffuse(diffuse), specular(specular), shininess(shininess) {}
