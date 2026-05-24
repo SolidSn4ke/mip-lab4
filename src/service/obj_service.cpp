8629a4de81cc1fc9eb2756f5ff47ac060c4b3816
vvlaads <vvlaads@gmail.com>
2026-05-24 14:15:17 +0300

    Refactored obj_service: removed unnecessary methods, added documentation, simplified code

--- src/service/obj_service.cpp
+++ src/service/obj_service.cpp
@@ -2,15 +2,6 @@
 #include <iostream>
 #include <sstream>
 
-static int parseIndex(const std::string& token) {
-    // Берём только часть до '/'
-    size_t pos = token.find('/');
-    if (pos == std::string::npos)
-        return std::stoi(token);
-
-    return std::stoi(token.substr(0, pos));
-}
-
 std::vector<Triangle> OBJService::parse_obj(const std::string& obj_content) {
     std::vector<Vector> vertices;
     std::vector<Triangle> triangles;
@@ -20,6 +11,11 @@ std::vector<Triangle> OBJService::parse_obj(const std::string& obj_content) {
     std::string line;
 
     while (std::getline(file, line)) {
+
+        // Пропускаем пустые строки и комментарии
+        if (line.empty() || line[0] == '#')
+            continue;
+
         std::istringstream ss(line);
         std::string type;
         ss >> type;
@@ -28,17 +24,14 @@ std::vector<Triangle> OBJService::parse_obj(const std::string& obj_content) {
         if (type == "v") {
             float x, y, z;
             ss >> x >> y >> z;
+
             vertices.emplace_back(x, y, z);
         }
 
         // Треугольники
         else if (type == "f") {
-            std::string t1, t2, t3;
-            ss >> t1 >> t2 >> t3;
-
-            int i1 = parseIndex(t1);
-            int i2 = parseIndex(t2);
-            int i3 = parseIndex(t3);
+            int i1, i2, i3;
+            ss >> i1 >> i2 >> i3;
 
             triangles.emplace_back(vertices[i1 - 1], vertices[i2 - 1], vertices[i3 - 1]);
         }
--- src/service/obj_service.h
+++ src/service/obj_service.h
@@ -5,8 +5,25 @@
 #include <string>
 #include <vector>
 
+/**
+ * @brief Сервис для парсинга OBJ файлов.
+ */
 class OBJService {
-
   public:
+    /**
+     * @brief Конвертирует содержимое OBJ файла в массив треугольников.
+     *
+     * Поддерживаются:
+     * - вершины (`v`)
+     * - треугольные полигоны (`f`)
+     *
+     * Не поддерживаются:
+     * - нормали (`vn`)
+     * - текстурные координаты (`vt`)
+     * - полигоны более чем с 3 вершинами
+     *
+     * @param obj_content Содержимое OBJ файла.
+     * @return Массив треугольников.
+     */
     static std::vector<Triangle> parse_obj(const std::string& obj_content);
 };
