e507a8bb8e54cbce7e9e7d6859cbe326e1866df1
vvlaads <vvlaads@gmail.com>
2026-05-24 16:58:43 +0300

    Refactored color, added documentation, removed limits

--- src/math/color.cpp
+++ src/math/color.cpp
@@ -1,27 +1,16 @@
 #include "color.h"
 #include <algorithm>
 
-// Конструктор
-Color::Color(float r, float g, float b)
-    : r(std::min(std::max(r, 0.0f), 1.0f)), g(std::min(std::max(g, 0.0f), 1.0f)),
-      b(std::min(std::max(b, 0.0f), 1.0f)) {}
+Color::Color(float r, float g, float b) : r(r), g(g), b(b) {}
 
-// Конвертация в вектор
-Vector Color::toVector() const {
-    return Vector(r, g, b);
-}
-
-// Сложение цветов
 Color Color::operator+(const Color& other) const {
     return Color(r + other.r, g + other.g, b + other.b);
 }
 
-// Умножение на число
 Color Color::operator*(float k) const {
     return Color(r * k, g * k, b * k);
 }
 
-// Покомпонентное умножение цветов
 Color Color::operator*(const Color& other) const {
     return Color(r * other.r, g * other.g, b * other.b);
 }
