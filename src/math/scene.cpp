c539884329306c46af215754968b8cdd2ef33b03
vvlaads <vvlaads@gmail.com>
2026-05-25 11:15:22 +0300

    Refactored scene: added documentation and comments

--- src/math/scene.cpp
+++ src/math/scene.cpp
@@ -1,5 +1,4 @@
 #include "scene.h"
 
-// Конструктор
 Scene::Scene(const std::vector<Triangle>& triangles, const std::vector<LightSource> lights)
     : triangles(triangles), lights(lights) {}
--- src/math/scene.h
+++ src/math/scene.h
@@ -4,11 +4,33 @@
 #include "triangle.h"
 #include <vector>
 
+/**
+ * @brief 3D сцена для рендеринга.
+ *
+ * Содержит геометрию сцены и источники света.
+ */
 class Scene {
   public:
+    /**
+     * @brief Треугольники сцены.
+     *
+     * Используются как основная геометрия
+     * для пересечения лучей.
+     */
     std::vector<Triangle> triangles;
+
+    /**
+     * @brief Источники света сцены.
+     *
+     * Используются при вычислении освещения.
+     */
     std::vector<LightSource> lights;
 
-    // Конструктор
+    /**
+     * @brief Создаёт сцену.
+     *
+     * @param triangles геометрия сцены
+     * @param lights источники света
+     */
     Scene(const std::vector<Triangle>& triangles, const std::vector<LightSource> lights);
 };
